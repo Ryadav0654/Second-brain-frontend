f0b39ad191f83721dd84152e004661a744b3fb9f
Ravindra Yadav <yadavra2005@gmail.com>
2025-03-29 18:47:46 +0530

    some fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,36 @@
 "use client";
-import apiClient from "@/lib/apiClient";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+// import apiClient from "@/lib/apiClient";
+// import { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
+  // const router = useRouter();
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const getUserDetails = async () => {
-      try {
-        const res = await apiClient.get("/api/v1/user", {
-          withCredentials: true, // Ensure cookies are sent
-        });
+  // useEffect(() => {
+  //   const getUserDetails = async () => {
+  //     try {
+  //       const res = await apiClient.get("/api/v1/user", {
+  //         withCredentials: true, // Ensure cookies are sent
+  //       });
 
-        if (res.status === 200) {
-          router.push("/dashboard");
-        } else {
-          router.push("/signin");
-        }
-      } catch (error) {
-        console.error("Error fetching user details:", error);
-        router.push("/signin");
-      } finally {
-        setLoading(false);
-      }
-    };
+  //       if (res.status === 200) {
+  //         router.push("/dashboard");
+  //       } else {
+  //         router.push("/signin");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching user details:", error);
+  //       router.push("/signin");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    getUserDetails();
-  }, [router]);
+  //   getUserDetails();
+  // }, [router]);
 
-  if (loading) return <p className="text-white">Loading...</p>;
+  // if (loading) return <p className="text-white">Loading...</p>;
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
